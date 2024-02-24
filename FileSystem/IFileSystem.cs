446d827a1b9f68357638ebd96465e1677542fafe
kapilaku <37741394+kapilaku@users.noreply.github.com>
2024-02-24 19:28:47 +0800

    finish file system part

---
+++ FileSystem/IFileSystem.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.FileProviders;
+using System.IO;
+using System.Text;
+
+namespace FileSystem;
+
+public interface IFileSystem
+{
+    void ShowStructure(Action<int, string> print);
+    Task<string> ReadAllTextAsync(string path);
+}
+
+public class FileSystem : IFileSystem
+{
+    private readonly IFileProvider _fileProvider;
+
+    public FileSystem(IFileProvider fileProvider)
+    {
+        _fileProvider = fileProvider;
+    }
+
+    public async Task<string> ReadAllTextAsync(string path)
+    {
+        var fileInfo = _fileProvider.GetFileInfo(path);
+        Byte[] buffer;
+        if (fileInfo.Exists)
+        {
+            using (var stream = fileInfo.CreateReadStream())
+            {
+                buffer = new byte[stream.Length];
+                await stream.ReadAsync(buffer, 0, buffer.Length);
+                return Encoding.Default.GetString(buffer);
+
+            }
+        }
+        return null;
+    }
+
+    public void ShowStructure(Action<int, string> print)
+    {
+        int indent = -1;
+        Print("");
+
+        void Print(string subpath)
+        {
+            indent++;
+            foreach (var fileInfo in _fileProvider.GetDirectoryContents(subpath))
+            {
+                print(indent, fileInfo.Name);
+                if (fileInfo.IsDirectory)
+                {
+                    Print($@"{subpath}\{fileInfo.Name}");
+                }
+            }
+            indent--;
+        }
+    }
+}
---
+++ FileSystem/Program.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+
+namespace FileSystem;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        await MonitorFileChanges();
+    }
+
+    static async Task MonitorFileChanges()
+    {
+        using var fileProvider = new PhysicalFileProvider(@"c:\test\");
+        string? origin = null;
+        ChangeToken.OnChange(() => fileProvider.Watch("data.txt"), CallBack);
+        while (true)
+        {
+            File.WriteAllText(@"c:\test\data.txt", DateTime.UtcNow.ToString());
+            await Task.Delay(5000);
+        }
+        async void CallBack()
+        {
+            var stream = fileProvider.GetFileInfo("data.txt").CreateReadStream();
+            {
+                var buffer = new byte[stream.Length];
+                await stream.ReadAsync(buffer, 0, buffer.Length);
+                var current = Encoding.Default.GetString(buffer);
+                if (current != origin)
+                {
+                    Console.WriteLine(origin = current);
+                    // ? origin = current;
+                }
+            }
+        }
+    }
+
+    static async Task ReadFileContentAsync()
+    {
+        var content = await new ServiceCollection()
+            .AddSingleton<IFileProvider>(new PhysicalFileProvider(@"c:\test"))
+            .AddSingleton<IFileSystem, FileSystem>()
+            .BuildServiceProvider()
+            .GetService<IFileSystem>()
+            .ReadAllTextAsync("test.txt");
+
+        Console.WriteLine(content);
+
+        Debug.Assert(content == File.ReadAllText(@"c:\test\test.txt"));
+    }
+
+    static void ReadDirectories()
+    {
+        new ServiceCollection()
+            .AddSingleton<IFileProvider>(new PhysicalFileProvider(@"c:\Users\Public"))
+            .AddSingleton<IFileSystem, FileSystem>()
+            .BuildServiceProvider()
+            .GetService<IFileSystem>()
+            .ShowStructure(Print);
+    }
+
+    static async Task ReadEmbeddedFileAsync()
+    {
+        var content = await new ServiceCollection()
+            .AddSingleton<IFileProvider>(new EmbeddedFileProvider(Assembly.GetEntryAssembly()))
+            .AddSingleton<IFileSystem, FileSystem>()
+            .BuildServiceProvider()
+            .GetService<IFileSystem>()
+            .ReadAllTextAsync("test.txt");
+
+        Console.WriteLine(content);
+
+        Debug.Assert(content == File.ReadAllText(@"c:\test\test.txt"));
+    }
+
+    static void Print(int layer, string name) => Console.WriteLine($"{new string(' ', layer * 4)}{name}");
+}
